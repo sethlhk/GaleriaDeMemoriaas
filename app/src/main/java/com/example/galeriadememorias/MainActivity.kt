85d8834923617cb549c3844f60c4fab57ff9ff05
PedroFrois29 <130504540+PedroFrois29@users.noreply.github.com>
2023-06-22 14:46:57 -0300

    R6UjjR

--- app/src/main/java/com/example/galeriadememorias/MainActivity.kt
+++ app/src/main/java/com/example/galeriadememorias/MainActivity.kt
@@ -7,19 +7,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Card
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -58,21 +64,32 @@ fun GaleriaMemorias(){
 
 @Composable
 fun GaleriaDeMemorias(IdImageDescricao:IdImageDescricao){
- Card() {
+ Card(
+     modifier = Modifier
+         .fillMaxWidth()
+         .padding(10.dp),
+     colors = CardDefaults.cardColors(Color(202, 225, 255)),
+     elevation = CardDefaults.cardElevation(5.dp)
 
-     Column() {
+ ) {
+     Column(
+         verticalArrangement = Arrangement.Top,
+         horizontalAlignment = Alignment.CenterHorizontally,
+         modifier = Modifier.fillMaxWidth()
+     ) {
 
       Image(painter = painterResource(id = IdImageDescricao.idImage)
           , contentDescription = null,
           contentScale =  ContentScale.Companion.Crop,
           modifier = Modifier
               .size(300.dp)
+              .padding(10.dp)
       )
 
               (Text(text = stringResource(id = IdImageDescricao.idTexto),
                   fontSize = 25.sp,
                   modifier = Modifier
-                      .size(5.dp)
+                      .padding(3.dp)
               ))
 
         }
