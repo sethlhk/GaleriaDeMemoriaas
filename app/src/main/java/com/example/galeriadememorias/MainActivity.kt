76bf946e34dc6579e4444e56d22d2deb46e96b93
PedroFrois29 <130504540+PedroFrois29@users.noreply.github.com>
2023-06-22 16:13:31 -0300

    R6UjjR

--- app/src/main/java/com/example/galeriadememorias/MainActivity.kt
+++ app/src/main/java/com/example/galeriadememorias/MainActivity.kt
@@ -6,9 +6,14 @@ package com.example.galeriadememorias
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.Card
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -23,10 +28,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -64,6 +74,7 @@ fun GaleriaMemorias(){
 
 @Composable
 fun GaleriaDeMemorias(IdImageDescricao:IdImageDescricao){
+    var expandir by remember { mutableStateOf(false) }
  Card(
      modifier = Modifier
          .fillMaxWidth()
@@ -75,7 +86,14 @@ fun GaleriaDeMemorias(IdImageDescricao:IdImageDescricao){
      Column(
          verticalArrangement = Arrangement.Top,
          horizontalAlignment = Alignment.CenterHorizontally,
-         modifier = Modifier.fillMaxWidth()
+         modifier = Modifier
+             .fillMaxWidth()
+             .animateContentSize(
+                 animationSpec = spring(
+                     dampingRatio = Spring.DampingRatioLowBouncy,
+                     stiffness = Spring.StiffnessMedium
+                 )
+             )
      ) {
 
       Image(painter = painterResource(id = IdImageDescricao.idImage)
@@ -84,13 +102,30 @@ fun GaleriaDeMemorias(IdImageDescricao:IdImageDescricao){
           modifier = Modifier
               .size(300.dp)
               .padding(10.dp)
-      )
+              .clickable {
 
+                  expandir = !expandir
+
+              }
+      )
               (Text(text = stringResource(id = IdImageDescricao.idTexto),
                   fontSize = 25.sp,
                   modifier = Modifier
                       .padding(3.dp)
               ))
+         if ( expandir==true) {
+
+            Spacer(modifier = Modifier.size(20.dp))
+             Text(
+                 text = "minhas melhores memórias",
+                 modifier =  Modifier.fillMaxWidth(),
+                 textAlign = TextAlign.Center
+
+             )
+         }
+
+
+
 
         }
     }
